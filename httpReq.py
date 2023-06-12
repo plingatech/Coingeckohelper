7df091a2cc70b74f7013da9ca3f7cf7e560cbb54
git <eae966@gmail.com>
2023-06-12 18:31:32 +0330

    Some code improvements.

--- httpReq.py
+++ httpReq.py
@@ -1,10 +1,15 @@
 import requests
+import time
 
 def send_get_request(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # بررسی کد وضعیت HTTP
-
-        return response.text
-    except requests.exceptions.RequestException as e:
-        print('Error:', e)
+    while True:
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # بررسی کد وضعیت HTTP
+            return response.text
+        except requests.exceptions.RequestException as e:
+            if '429' in str(e):
+                print('Too Many Requests. Waiting for 5 seconds...')
+                time.sleep(5)
+            else:
+                print('Error:', e)
